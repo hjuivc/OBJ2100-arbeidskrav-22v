3751415fe9418294dc267eab4c5be53aa98e0ee1
Bjørnar Borge <72221999+bjbogithub@users.noreply.github.com>
2022-03-25 13:21:40 +0100

    Create Controller.java

---
+++ src/Grensesnitt/Controller.java
@@ -0,0 +1,30 @@
+package Grensesnitt;
+
+import javafx.fxml.FXML;
+import javafx.scene.control.Button;
+import javafx.scene.control.Label;
+
+public class Controller {
+	@FXML
+	private Button kunde;
+	
+	@FXML
+	private Button planlegger;
+	
+	@FXML
+	private Button kinobetjent;
+	
+	@FXML
+	private Label test;
+	
+	// Lytter
+	@FXML
+	public void behandleKlikk() {
+	}
+	
+	@FXML
+	public void Initialize() {
+		
+	}
+
+}
